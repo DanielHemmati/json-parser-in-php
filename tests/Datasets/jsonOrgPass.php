6e4cedbdaf0160ce16a530af544f0829452e42a8
DanielHemmati <danialups1997@gmail.com>
2025-06-28 21:08:12 +0200

    refactor: remove true from file_get_contents, it's un-necessary

--- tests/Datasets/jsonOrgPass.php
+++ tests/Datasets/jsonOrgPass.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 dataset('jsonOrgPass', function () {
     $pattern = __DIR__ . '/../JSONOrg/pass*.json';
     foreach (glob($pattern) as $file) {
-        $json = file_get_contents($file, true);
+        $json = file_get_contents($file);
         $expected = json_decode($json, true);
         yield basename($file) => [$json, $expected];
     }
